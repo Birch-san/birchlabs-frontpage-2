afb5e2e501eb3e400dc5e7e08f64a8655e612967
Alex Birch <vengeance.mx@gmail.com>
2016-03-10 00:13:12 +0000

    simplify

--- client/lib/defer-until-loaded/index.js
+++ client/lib/defer-until-loaded/index.js
@@ -1,5 +1,6 @@
-module.exports = function(callback) {
-	(window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.addEventListener.bind(null, 'load'))(function() {
-		callback();
-	});
-};
+module.exports = (window.requestAnimationFrame
+	|| window.mozRequestAnimationFrame
+	|| window.webkitRequestAnimationFrame
+	|| window.msRequestAnimationFrame
+	|| window.addEventListener.bind(null, 'load')
+	);
